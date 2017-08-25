251ba4e080a06346a1f54e779dd80ec54dace58d
Shrimadhav U K <spechide@shrimadhavuk.me>
2017-08-25 14:33:23 +0530

    noyes Dump

--- js/Z.exec.js
+++ js/Z.exec.js
@@ -2,3 +2,27 @@
 var SearchFormOnSubmit = function(){
 	console.log("not implemented");
 };
+
+var AboutMe = function(){
+	alert("about me blah blah blah");
+};
+
+var ContactMe = function(){
+	alert('contact me');
+};
+
+var MyEdu = function(){
+	alert("education");
+};
+
+var MyFriendsList = function(){
+	alert("NO");
+};
+
+var MyHobbies = function(){
+	alert("NOYES.IN");
+};
+
+var MyWork = function(){
+	alert("All work and No play makes me a dull boy");
+};
